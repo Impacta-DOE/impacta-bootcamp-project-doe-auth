42cf39090be0ee973d8c6aeb6618ed472861b211
mateus <m.mateus.moreira10@gmail.com>
2021-11-10 20:17:35 -0300

    implementacao do jwt e login

---
+++ src/test/java/com/impacta/bootcamp/project/doe/auth/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impacta.bootcamp.project.doe.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
