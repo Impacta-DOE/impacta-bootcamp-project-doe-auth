53eda5b0044e0daa4ea07431004349f1e8f7182f
Arthur <santosalmeida.arthur@gmail.com>
2022-01-08 18:11:53 -0300

    feat: adicionando endpoint para buscar username por id

--- src/main/java/com/impacta/bootcamp/project/doe/auth/controller/UserController.java
+++ src/main/java/com/impacta/bootcamp/project/doe/auth/controller/UserController.java
@@ -3,10 +3,7 @@ package com.impacta.bootcamp.project.doe.auth.controller;
 import com.impacta.bootcamp.project.doe.auth.controller.dto.UserDto;
 import com.impacta.bootcamp.project.doe.auth.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/usuario")
@@ -18,4 +15,10 @@ public class UserController {
     public UserDto salva(@RequestBody UserDto dto) {
         return userService.salva(dto);
     }
+
+    @GetMapping("/username/{id}")
+    public String buscaUsernamePorIdDoUsuario(@PathVariable String id){
+        return userService.buscaUsernamePor(id);
+    }
+
 }
